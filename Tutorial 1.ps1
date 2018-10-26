7dcd51cc09a1eaeeabaed7a532cc2b3e385d23dc
vinayakvshinde <44312215+vinayakvshinde@users.noreply.github.com>
2018-10-26 10:21:43 -0500

    Tutorial 1
    Task 1

---
+++ Tutorial 1.ps1
@@ -0,0 +1,19 @@
+﻿#region Command to produce random numbers
+Get-Command -Noun Random
+Get-help Get-Random -Examples
+Get-help Get-Random -Detailed
+Get-Random
+#Command to display current date and time
+Get-Command -Noun Date
+Get-help Get-Date -Examples
+$mydate=Get-Date
+$mydate.GetType()
+$mydate.DateTime
+$mydate.DateTime |Get-Member
+#Command to display hotfix
+Get-Command -Noun Hotfix
+Get-HotFix
+#Command to display list of 50 newest entries from security log
+Get-Help Get-EventLog -Detailed
+Get-EventLog -LogName Security -Newest 50
+#endregion
