39cc42ab38927e630d25da33a3994df16fa93f4d
vinayakvshinde <44312215+vinayakvshinde@users.noreply.github.com>
2018-12-28 23:42:50 +0530

    Task_28 Dec
    Task_28 Dec

---
+++ Task_28Dec.ps1
@@ -0,0 +1,28 @@
+﻿############################ Task to calculate number ####################################
+Function Get-Numberdetail
+{
+
+[CmdletBinding()]
+[outputtype([INT])]
+
+Param (  
+
+            [Parameter (mandatory=$true,Valuefrompipelinebypropertyname=$true,Position=0)]
+
+            [Int]$a,
+
+            [Parameter (mandatory=$true,Valuefrompipelinebypropertyname=$true,Position=1)]
+
+            [INT]$b
+
+      )
+
+      $a+$b
+      $a-$b
+      $a/$b
+      $a*$b
+
+
+}
+
+Get-Numberdetail 20 10
