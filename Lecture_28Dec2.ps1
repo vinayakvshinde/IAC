59535d494517f2717c078f59e56f97b4b9b6d934
vinayakvshinde <44312215+vinayakvshinde@users.noreply.github.com>
2018-12-29 16:27:21 +0530

    Lecture_28Dec 2
    Lecture_28Dec 2

---
+++ Lecture_28Dec2.ps1
@@ -0,0 +1,16 @@
+﻿New-ModuleManifest -Path "C:\Users\Administrator\Documents\WindowsPowerShell\Modules\getinfo\getinfo.psd1"
+$env:PSModulePath
+Get-Module -ListAvailable
+Import-Module -Name Getinfo
+Get-Info
+$PSVersionTable
+Remove-Module -Name getinfo
+Get-Command -Module getinfo
+
+New-ModuleManifest -Path "C:\Users\Administrator\Documents\WindowsPowerShell\Modules\writehello\writehello.psd1"
+Import-Module -Name "writehello" -Force
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+Write-Hello
+Get-Help Import-Module -ShowWindow
+Import-Module -Name 'writehello' -Force
+
