4c331a30f9ee2fbdd1a9e46ff7784097f9922acb
vinayakvshinde <44312215+vinayakvshinde@users.noreply.github.com>
2018-12-29 14:51:08 +0530

    Lecture_29Dec
    Lecture_29Dec

---
+++ Lecture_29Dec.ps1
@@ -0,0 +1,141 @@
+﻿############################################################################
+
+function Get-AzureVM1
+{
+    [CmdletBinding()]
+
+    Param ( 
+    
+            $VMname,
+            $Region
+
+            )
+
+
+  Begin { "Starting" }
+  Process { Write-Host "HI"}
+  End  { "Ending" }
+
+
+}
+
+Get-AzureVM1 -Verbose
+
+###########################Set Verbose###################
+function Get-AzureVM2
+{
+    [CmdletBinding()]
+
+    Param ( 
+    
+            $VMname,
+            $Region
+
+            )
+
+
+  Begin { "Starting" }
+  Process { Write-verbose "Trying to connect $Comp"
+
+            Test-Connection
+            Write-Verbose " Fetching inventory"
+            
+            Write-host "HI" }
+  End  { "Ending" }
+
+
+}
+
+Get-AzureVM2
+Get-AzureVM2 -Verbose
+
+### Write-Debug / Write-Warning ---> Need to check at home ###
+
+#####################Error Handling ###########################
+
+$ErrorActionPreference
+$Error
+
+##############Try & Catch work in case of error action stop ##################
+
+Try 
+
+{ Get-Vinayak
+
+}
+
+Catch
+
+{
+
+Write-Host " This error is $_"
+
+}
+
+Try 
+
+{ Get-Service -Name "Prath"
+
+}
+
+Catch
+
+{
+
+Write-Host " This error is $_"
+
+}
+
+Try 
+
+{ Get-Service -Name "Prath" -ErrorAction Stop
+
+}
+
+Catch
+
+{
+
+Write-Host " This error is $_"
+
+}
+
+Try 
+
+{ Get-Service -Name "Prath" -ErrorAction Silentlycontinue
+
+}
+
+Catch
+
+{
+
+Write-Host " This error is $_"
+
+}
+
+Try 
+
+{ Test-Connection -ComputerName "TestVM" -ErrorAction Stop
+
+}
+
+Catch
+
+{
+
+Write-Host " This error is $_"
+
+$_.Categoryinfo
+#$_.Exception
+#$_.errordetails
+
+
+}
+
+####################### Define Modules #######################
+
+
+
+
+
